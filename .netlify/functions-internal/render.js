72c597774f281cde0c0b71b753213c7b982c98d5
Theo Jolliffe <theojolliffe@gmail.com>
2022-08-01 20:26:48 +0100

    first commit

---
+++ .netlify/functions-internal/render.js
@@ -0,0 +1,32 @@
+const { init } = require('../serverless.js');
+
+exports.handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-08885b2e.js","imports":["_app/immutable/start-08885b2e.js","_app/immutable/chunks/index-03969575.js"],"stylesheets":[]},
+		nodes: [
+			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				type: 'page',
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				path: "/",
+				shadow: null,
+				a: [0,2],
+				b: [1]
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
